2ab518c246598c7462514966f32626ea23b1b4ff
Edilson Pereira da Silva <di3.pereiradasilva@gmail.com>
2018-08-16 23:51:33 -0300

    Criando duas páginas e possibilitando a navegação

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+@IonicPage()
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  login() {
+    // this.navCtrl.push('CategoriasPage');
+    this.navCtrl.setRoot('CategoriasPage');
+  }
 }
