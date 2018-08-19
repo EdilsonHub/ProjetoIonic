1760a1e2dc7e654df3d05ba9bc6c314db28d5d5d
Edilson Pereira da Silva <di3.pereiradasilva@gmail.com>
2018-08-19 20:44:23 -0300

    listando categorias no console

---
+++ src/app/services/categoria.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { CategoriaDTO } from "../models/categoria.dto";
+import { Observable } from "rxjs/Observable";
+import { API_CONFIG } from "../../config/api.config";
+
+@Injectable()
+export class CategoriaService {
+
+    categorias :CategoriaDTO[];
+    constructor(public http: HttpClient){}
+
+    findAll() : Observable<CategoriaDTO[]> {
+        console.log(`${API_CONFIG.baseUrl}/categorias`);
+        return this.http.get<CategoriaDTO[]>(`${API_CONFIG.baseUrl}/categorias`);
+    }
+}
