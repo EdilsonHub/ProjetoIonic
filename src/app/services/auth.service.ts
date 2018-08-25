6101b07e9cac5dddf74034f65b2bf3e2d56b85fd
Edilson Pereira da Silva <di3.pereiradasilva@gmail.com>
2018-08-25 19:48:48 -0300

    Extraindo o email do token e armazenando-o em localStorage

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,11 +4,13 @@ import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../../config/api.config";
 import { StorageService } from "./storage.service";
 import { LocalUser } from "../models/localUser.dto";
-import { STORAGE_KEYS } from "../../config/storage_key.config";
+import { JwtHelper } from "angular2-jwt";
 
 @Injectable()
 export class AuthService {
     
+    jwtHelper: JwtHelper = new JwtHelper();
+
     constructor(
         private http: HttpClient,
         private stogeService: StorageService
@@ -25,7 +27,8 @@ export class AuthService {
     successfullLogin(autorizationToken: string) {
  
         let usr: LocalUser = {
-            token : autorizationToken.substring(7)
+            token: autorizationToken.substring(7),
+            email: this.jwtHelper.decodeToken(autorizationToken.substring(7)).sub
         }
 
         this.stogeService.setLocalUser(usr);
@@ -35,3 +38,11 @@ export class AuthService {
         this.stogeService.setLocalUser(null);
     }
 }
+
+// /** sudo npm install --save angular2-jwt
+//  * -------------------------------------
+// npm WARN angular2-jwt@0.2.3 requires a peer of @angular/core@^2.0.0||^4.0.0 but none is installed. You must install peer dependencies yourself.
+// npm WARN angular2-jwt@0.2.3 requires a peer of @angular/http@^2.0.0||^4.0.0 but none is installed. You must install peer dependencies yourself.
+// npm WARN optional SKIPPING OPTIONAL DEPENDENCY: fsevents@1.2.4 (node_modules/fsevents):
+// npm WARN notsup SKIPPING OPTIONAL DEPENDENCY: Unsupported platform for fsevents@1.2.4: wanted {"os":"darwin","arch":"any"} (current: {"os":"linux","arch":"x64"})
+// * /
